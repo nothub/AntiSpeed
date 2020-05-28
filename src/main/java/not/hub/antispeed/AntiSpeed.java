25909212b8fa55a1799015a3962f7118e782fd44
blockparole <48992448+blockparole@users.noreply.github.com>
2020-05-28 23:06:57 +0200

    unregister handlers and cancel tasks in onDisable

--- src/main/java/not/hub/antispeed/AntiSpeed.java
+++ src/main/java/not/hub/antispeed/AntiSpeed.java
@@ -6,9 +6,11 @@ import org.apache.logging.log4j.Logger;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -20,13 +22,14 @@ import java.util.stream.Collectors;
 public final class AntiSpeed extends JavaPlugin implements Listener {
 
     // TODO: clear player data on leave event or keep for distance diff compare after relog?
+    // TODO: plugman compatibility https://github.com/r-clancy/PlugMan/issues/68
 
     public static final Logger LOGGY = LogManager.getLogger("AntiSpeed");
 
     Map<UUID, Location> lastLocations;
     Map<UUID, EvictingQueue<Double>> historicalDistances;
 
-    public void resetPlayer(Player player) {
+    public void resetPlayerData(Player player) {
         lastLocations.put(player.getUniqueId(), player.getLocation());
         historicalDistances.put(player.getUniqueId(), EvictingQueue.create(10));
     }
@@ -34,7 +37,7 @@ public final class AntiSpeed extends JavaPlugin implements Listener {
     public void calcPlayerDistanceDiff(Player player) {
 
         if (!lastLocations.containsKey(player.getUniqueId()) || !lastLocations.get(player.getUniqueId()).getWorld().equals(player.getWorld())) {
-            resetPlayer(player);
+            resetPlayerData(player);
             return;
         }
 
@@ -59,16 +62,27 @@ public final class AntiSpeed extends JavaPlugin implements Listener {
 
         getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
 
-            getServer().getOnlinePlayers().forEach(this::calcPlayerDistanceDiff);
+            getServer().getOnlinePlayers().forEach(player -> {
 
-            // TODO: do action on speed violation
-            LOGGY.info(historicalDistances.toString());
-            historicalDistances.forEach((uuid, doubles) ->
-                    LOGGY.info(uuid + " average distance diff: " + doubles.stream().collect(
-                            Collectors.summarizingDouble(Double::doubleValue))));
+                LOGGY.info("Calculating Distance for: " + player.getName());
+
+                calcPlayerDistanceDiff(player);
+
+                LOGGY.info("Historical Distances for " + player.getName() + ": " + historicalDistances.get(player.getUniqueId()));
+                LOGGY.info("Average Distance Diff for " + player.getName() + ": " + historicalDistances.get(player.getUniqueId()).stream().collect(Collectors.summarizingDouble(Double::doubleValue)));
+
+                // TODO: do action on speed violation
+
+            });
 
         }, 0L, 10L);
 
+    }
+
+    @Override
+    public void onDisable() {
+        HandlerList.unregisterAll((Plugin) this);
+        getServer().getScheduler().cancelTasks(this);
     }
 
     @EventHandler
@@ -81,7 +95,7 @@ public final class AntiSpeed extends JavaPlugin implements Listener {
         LOGGY.info("Resetting stats for " + playerTeleportEvent.getPlayer().getName() + " because of PlayerTeleportEvent (" + playerTeleportEvent.getCause().toString() + ")");
 
         getServer().getScheduler().scheduleSyncDelayedTask(this, () ->
-                resetPlayer(playerTeleportEvent.getPlayer()
+                resetPlayerData(playerTeleportEvent.getPlayer()
                 ), 1L);
 
     }
@@ -92,7 +106,7 @@ public final class AntiSpeed extends JavaPlugin implements Listener {
         LOGGY.info("Resetting stats for " + playerRespawnEvent.getPlayer().getName() + " because of PlayerRespawnEvent");
 
         getServer().getScheduler().scheduleSyncDelayedTask(this, () ->
-                resetPlayer(playerRespawnEvent.getPlayer()
+                resetPlayerData(playerRespawnEvent.getPlayer()
                 ), 1L);
 
     }
